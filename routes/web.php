42bcad7e3292f56bcb877ca2ba55804cdb2c937c
RUHTRA16 <ruhtraff2022@gmail.com>
2025-09-05 22:38:36 -0300

    atualização_rotas

--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,26 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', [\App\Http\Controllers\home_controller::class, 'home']);
 
 Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'sobreNos']);
 
 Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato']);
+
+Route::get (
+    '/contato/{nome}/{categoria}/{assunto}/{mensagem?}',
+    function(string $nome, string $categoria, string $assunto, string $mensagem = "Mensagem não preenchida"){
+       echo "Estamos aqui: $nome - $categoria - $assunto - $mensagem";
+    }
+);
+
+Route::get (
+    '/contato/{nome}/{categoria_id}',
+    function(
+        string $nome = 'Desconhecido',
+             int $categoria_id = 1 // 1 - Categoria
+             ) {
+        echo "Estamos aqui: $nome - $categoria_id";
+             }
+)->where('categoria_id','[0-9]+',)->where('nome', '[A-Za-z]+');
