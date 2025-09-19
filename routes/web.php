c3890a8f1cbdd97653a9633c2f23928d6faac3a7
RUHTRA16 <ruhtraff2022@gmail.com>
2025-09-19 11:51:55 -0300

    redirecionamento de rotas e fallback

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,17 @@ Route::prefix('/app')->group(function(){
     Route::get('/clientes', function(){return 'clientes';})->name('app.clientes');
     Route::get('/produtos', function(){return 'produtos';})->name('app.produtos');
 });
+
+Route::get('/rota1', function(){
+    echo 'Rota 1';
+})->name('site.rota1');
+
+Route::get('/rota2', function(){
+    return redirect()->route('site.rota1');
+})->name('site.rota2');
+
+//Route::redirect('/rota2','/rota1');
+
+Route::fallback(function(){
+    echo 'A rota acessada não existe. <a href="'.route('Página_Inicial').'">Clique aqui</a> para ir para a página inicial';
+});
