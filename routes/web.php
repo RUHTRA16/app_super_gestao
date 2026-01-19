6b543133696e8ff9584c48e7ed0193aa51ee69f0
RUHTRA16 <ruhtraff2022@gmail.com>
2026-01-19 18:07:09 -0300

    adição da navbar

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [\App\Http\Controllers\home_controller::class, 'home'])->name('Página_Inicial');
 Route::get('/sobreNos', [\App\Http\Controllers\SobreNosController::class, 'sobreNos'])->name('Sobre-Nós');
 Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato'])->name('Contato');
-Route::get('/login', function(){ return 'login';});
+Route::get('/login', [\App\Http\Controllers\LoginController::class, 'login'])->name('Login');
 
 Route::prefix('/app')->group(function(){
     Route::get('/fornecedores', function(){return 'fornecedores';})->name('app.fornecedores');
