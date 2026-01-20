88f357c95b44df307852fae8a8b2a2590f619077
RUHTRA16 <ruhtraff2022@gmail.com>
2026-01-20 17:08:21 -0300

    melhoramento navbar

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -3,27 +3,59 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>CNS - Libras</title>
 
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+
+<!-- NAVBAR -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+  <div class="container-fluid">
+    <a class="navbar-brand d-flex align-items-center">
+      <img src="{{ asset('images/logosite.png') }}"
+           alt="Logo"
+           height="40"
+           class="d-inline-block align-text-top">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/">Página Inicial</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/contato">Contato</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/sobreNos">Sobre-Nós</a>
+        </li>
+      </ul>
+      <div class="ms-auto">
+        <a href="/login" class="btn btn-outline-light">
+          Entrar
+        </a>
+      </div>
+    </div>
+  </div>
+</nav>
+
 </head>
 <body>
-    <body class="d=flex ">
-<h3>Principal (view)</h3>
+    
+<div class="hero">
+  <div class="hero-left">
+    <h1>Bem-vindo ao CNS Libras</h1>
+    <p>Conteúdo do site aqui</p>
+  </div>
 
-<ul>
-    <li>
-        <a href="/">Principal</a>
-    </li>
-    <li>
-        <a href="contato">Contato</a>
-    </li>
-    <li><a href="sobreNos">Sobre-Nós</a>
-    </li>
-</ul>
+   <div class="hero-right"></div>
+</div>
 
+
+<!--BOOTSTRAP-->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
 </html>
-
